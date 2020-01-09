a6684f55062c0cfdbbab1e955014531d638f780e
John Ong <john.ong135@gmail.com>
2020-01-08 20:22:40 -0800

    Gigantic rework of the website using React Bootstrap as well asReact spring to make the website more responsive

--- johnoportfolio/src/App.js
+++ johnoportfolio/src/App.js
@@ -1,24 +1,86 @@
-import React from 'react';
-import './App.css';
-import Nav from './Nav';
-import About from './About';
-import Home from './Home';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Switch>
-          <Route path = "/" exact component = {Home}/>
-          <Route path = "/About" component = {About}/>
-        </Switch>
-        
-      </div>
-    </Router>
-   
-  );
+import Footer from "./components/Footer.js";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "John Ong",
+      headerLinks: [
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" }
+      ],
+      home: {
+        title: "Staying Hungry",
+        subTitle: "Always a student",
+        text: "Checkout my projects"
+      },
+      about: {
+        title: "About me"
+      },
+      contact: {
+        title: "Let's Talk"
+      }
+    };
+  }
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>John Ong</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <HomePage
+                title={this.state.home.title}
+                subTitle={this.state.home.subTitle}
+                text={this.state.home.text}
+              />
+            )}
+          />
+          <Route
+            path="/about"
+            exact
+            render={() => <AboutPage title={this.state.about.title} />}
+          />
+          <Route
+            path="/contact"
+            exact
+            render={() => <ContactPage title={this.state.contact.title} />}
+          />
+          <Footer />
+        </Container>
+      </Router>
+    );
+  }
 }
-
 export default App;
---
+++ johnoportfolio/src/components/Carousel.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+import LookNCook from "../images/LookNCook.png";
+import ProPic from "../images/ProPic.JPG";
+import PyramidScheme from "../images/PyramidScheme.PNG";
+
+import Card from "../components/Card.js";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 0,
+          title: "Look-N-Cook",
+          subTitle: "Senior Project in progess",
+          imgSrc: LookNCook,
+          link: "https://look-n-cook.firebaseapp.com",
+          selected: false
+        },
+        {
+          id: 1,
+          title: "My Github",
+          subTitle: "My projects big and small",
+          imgSrc: ProPic,
+          link: "https://github.com/JohnO135",
+          selected: false
+        },
+        {
+          id: 2,
+          title: "My hobbies",
+          subTitle: "Check out my crew",
+          imgSrc: PyramidScheme,
+          link: "https://www.youtube.com/watch?v=4yCOhjTD9-4",
+          selected: false
+        }
+      ]
+    };
+  }
+
+  handleCardClick = (id, card) => {
+    let items = [...this.state.items];
+
+    items[id].selected = items[id].selected ? false : true;
+
+    items.forEach(item => {
+      if (item.id !== id) {
+        item.selected = false;
+      }
+    });
+
+    this.setState({
+      items
+    });
+  };
+
+  makeItems = items => {
+    return items.map(item => {
+      return (
+        <Card
+          item={item}
+          click={e => this.handleCardClick(item.id, e)}
+          key={item.id}
+        />
+      );
+    });
+  };
+
+  render() {
+    return (
+      <Container fluid={true}>
+        <Row className="justify-content-around">
+          {this.makeItems(this.state.items)}
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Carousel;
